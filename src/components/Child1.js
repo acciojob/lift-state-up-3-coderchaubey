f8cf515654cdda7a8770fdd12c351f977fd4a800
coderchaubey <sunnychaubeysm@gmail.com>
2023-06-15 01:50:46 +0530

    changed content

--- src/components/Child1.js
+++ src/components/Child1.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Child1({setSelectedOption}) {
   return (
     <div className='child component 1'>
-      <button onClick={(e) => setSelectedOption(e.target.textContent)}>Child Component 1</button>
+      <button onClick={(e) => setSelectedOption(e.target.textContent)}>Option 1</button>
     </div>
   )
 }
--- src/components/Child2.js
+++ src/components/Child2.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Child2({setSelectedOption}) {
     return (
       <div className='child component 2'>
-        <button onClick={(e) => setSelectedOption(e.target.textContent)}>Child Component 2</button>
+        <button onClick={(e) => setSelectedOption(e.target.textContent)}>Option 2</button>
       </div>
     )
   }
